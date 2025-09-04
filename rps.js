d6383d74674339695becc518cd8d33ea8a5c94e5
John Cassidy <jjcassidy18@icloud.com>
2025-09-04 08:15:16 -0400

    remove console game and switch to ui logic

--- rps.js
+++ rps.js
@@ -14,71 +14,70 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose your move (rock, paper, or scissors): ");
-    const VALID_CHOICE = ["rock", "paper", "scissors"];
-    
-    while(!VALID_CHOICE.includes(choice.toLowerCase())) {
-        choice = prompt("Invalid choice, please enter rock, paper or scissors: ")
-    }
-
-    return choice.toLowerCase();
-}
+let humanScore = 0;
+let computerScore = 0;
 
 
+function playRound(humanChoice, computerChoice) {
+    let msg;
+    const lossMsg = `You lose! ${computerChoice} beats ${humanChoice}`;
+    const tieMsg = `It's a tie! ${computerChoice} is the same as ${humanChoice}`;
+    const winMsg = `You Win! ${humanChoice} beats ${computerChoice}`;
+    const results = document.querySelector("#results");
 
-function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        let lossMsg = `You lose! ${computerChoice} beats ${humanChoice}`;
-        let tieMsg = `It's a tie! ${computerChoice} is the same as ${humanChoice}`;
-        let winMsg = `You Win! ${humanChoice} beats ${computerChoice}`;
-
-        if (humanChoice === computerChoice) {
-            humanScore++
-            computerScore++
-            console.log(tieMsg + `\nscore: ${humanScore} to ${computerScore}`);
-        } 
-        else if ((humanChoice === 'rock' && computerChoice == 'paper') 
-            || (humanChoice === 'scissors' && computerChoice == 'rock') 
-            || (humanChoice === 'paper' && computerChoice == 'scissors')) {
-                computerScore++;
-                console.log(lossMsg + `\nscore: ${humanScore} to ${computerScore}`);
-        }
-        else {
-            humanScore++;
-            console.log(winMsg + `\nscore: ${humanScore} to ${computerScore}`);
-        }
-    }
-
-    for(let i = 0; i < 5; i++) {
-        let humanEX = getHumanChoice();
-        let computerEX = getComputerChoice();
-
-        playRound(humanEX, computerEX);
-    }
-
-    console.log("\n");
-
-    let endMsg;
-
-    if (humanScore > computerScore) {
-        endMsg = `You won the game! The score was ${humanScore} to ${computerScore}`;
-    }
-    else if(humanScore < computerScore) {
-        endMsg = `You lost the game :( The score was ${humanScore} to ${computerScore}`;
+    if (humanChoice === computerChoice) {
+        humanScore++
+        computerScore++
+        msg = tieMsg;
+    } 
+    else if ((humanChoice === 'rock' && computerChoice == 'paper') 
+        || (humanChoice === 'scissors' && computerChoice == 'rock') 
+        || (humanChoice === 'paper' && computerChoice == 'scissors')) {
+            computerScore++;
+            msg = lossMsg;
     }
     else {
-        endMsg = `It's a tie! ${humanScore} to ${computerScore}`;
+        humanScore++;
+        msg = winMsg;
     }
 
-    return endMsg;
+    if(humanScore >= 5) {
+        msg = "YOU WIN THE GAME";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if(humanScore >= 5) {
+        msg = "YOU LOSE THE GAME";
+        humanScore = 0;
+        computerScore = 0;
+    }
+
+    results.textContent = msg;
+    
+    const displayHumanScore = document.querySelector(".human-score");
+    const displayComputerScore = document.querySelector(".computer-score");
+
+    displayHumanScore.textContent = humanScore;
+    displayComputerScore.textContent = computerScore;
 }
 
-console.log(playGame());
+
+
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice())
+});
+paperBtn.addEventListener("click",  () => {
+    playRound("paper", getComputerChoice())
+});
+scissorsBtn.addEventListener("click",  () => {
+    playRound("scissors", getComputerChoice())
+});
+
+
 
 
 
